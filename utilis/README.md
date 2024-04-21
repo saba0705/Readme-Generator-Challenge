55265a0e3128f6f30e8a329b3331bdb7121175c7
saba pervez <sabah.junaid@outlook.com>
2024-04-21 19:54:19 -0400

    fixed the bugs in index.js

---
+++ utilis/README.md
@@ -0,0 +1,32 @@
+# example title
+
+## Description
+example description
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Write your installation instructions here.
+
+## Usage
+Write usage information here.
+
+## License
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+This application is covered under the MIT license.
+
+## Contributing
+Write contribution guidelines here.
+
+## Tests
+Write test instructions here.
+
+## Questions
+For additional questions, please contact sample@outlook.com.
+GitHub: [sample0705](https://github.com/sample0705)
--- utilis/index.js
+++ utilis/index.js
@@ -1,14 +1,13 @@
+
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
-
+const generateMarkdown = require('./generateMarkdown');
 
 // TODO: Create an array of questions for user input
-
-inquirer
-.prompt([
-    { type: 'input',
+const questions = [
+  {
+    type: 'input',
     name: 'title',
     message: 'Enter the title of your project:',
   },
@@ -17,68 +16,63 @@ inquirer
     name: 'description',
     message: 'Enter a description for your project:',
   },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Enter installation instructions:',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Enter usage information:',
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Enter contribution guidelines:',
-    },
-    {
-        type: 'input',
-        name: 'test',
-        message: 'Enter test instructions:',
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license:',
-        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC'],
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub username:',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Enter your email address:',
-    },
-])
-// TODO: Create a function to write README file
-.then((data) => {
-    const README = 'README.md';
-    writeToFile(README, data);
-});
-function writeToFile(README, data) {
-    const content = generateMarkdown(data);
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Enter contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'Enter test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license:',
+    choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
 
-    fs.writeFile(README, content, (err) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log('README.md file has been successfully created!');
-        }
-    });
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  const content = generateMarkdown(data);
+
+  fs.writeFile(fileName, content, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`${fileName} file has been successfually created!`);
+    }
+  });
 }
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-        console.log(answers);
-    });
+  inquirer.prompt(questions).then((answers) => {
+    const README = 'README.md';
+    writeToFile(README, answers);
+  });
 }
 
 // Function call to initialize app
