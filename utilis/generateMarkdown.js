05a6173e62301b3147f938aa13ddafbe9e423173
saba pervez <sabah.junaid@outlook.com>
2024-04-21 18:41:06 -0400

    added code to index.js and generateMarkdown.js

--- utilis/generateMarkdown.js
+++ utilis/generateMarkdown.js
@@ -1,1 +1,83 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+ // If there is no license, return an empty string
+ 
+function renderLicenseBadge(license) {
+    if (license == 'MIT') {
+        return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    } else if (license == 'Apache 2.0') {
+        return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    } else if (license == 'GPL 3.0') {
+        return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    } else if (license == 'BSD 3') {
+        return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+    } else {
+        return ''
+    }
+}
 
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    if (license == 'MIT') {
+        return `[MIT License](https://opensource.org/licenses/MIT)`
+    } else if (license == 'Apache 2.0') {
+        return `[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)`
+    } else if (license == 'GPL 3.0') {
+        return `[GPL 3.0 License](https://www.gnu.org/licenses/gpl-3.0)`
+    } else if (license == 'BSD 3') {
+        return `[BSD 3 License](https://opensource.org/licenses/BSD-3-Clause)`
+    } else {
+        return ''
+    }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    if (license) {
+        return `This project is licensed under the ${license} license.`
+    } else {
+        return ''
+    }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Write your installation instructions here.
+
+## Usage
+Write usage information here.
+
+## License
+[![License](https://img.shields.io/badge/license-${data.license}-blue.svg)](LICENSE)
+This application is covered under the ${data.license} license.
+
+## Contributing
+Write contribution guidelines here.
+
+## Tests
+Write test instructions here.
+
+## Questions
+For additional questions, please contact ${data.email}.
+GitHub: [${data.github}](https://github.com/${data.github})
+`;
+}
+
+
+
+module.exports = generateMarkdown;
--- utilis/index.js
+++ utilis/index.js
@@ -1,1 +1,85 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
+
+// TODO: Create an array of questions for user input
+
+inquirer
+.prompt([
+    { type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description for your project:',
+  },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+])
+// TODO: Create a function to write README file
+.then((data) => {
+    const README = 'README.md';
+    writeToFile(README, data);
+});
+function writeToFile(README, data) {
+    const content = generateMarkdown(data);
+
+    fs.writeFile(README, content, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('README.md file has been successfully created!');
+        }
+    });
+}
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log(answers);
+    });
+}
+
+// Function call to initialize app
+init();
